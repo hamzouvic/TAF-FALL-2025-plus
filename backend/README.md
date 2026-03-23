8b426a64637de071c19b87cce7b256f6e8b984ba
HamzaAfif <HamzaAfif@users.noreply.github.com>
2026-03-23 05:07:36 +0000

    deploy: 02bae0ae69666f381e90441b97164d39bd125a80

--- backend/README.md
+++
@@ -1,6 +0,0 @@
-# Backend API Documentation
-
-The REST API documentation is available via the running backend at:
-```
-http://localhost:8080/swagger-ui.html
-```
--- frontend/js/menu-wc.js
+++ frontend/js/menu-wc.js
@@ -15,7 +15,7 @@ customElements.define('compodoc-menu', class extends HTMLElement {
         <nav>
             <ul class="list">
                 <li class="title">
-                    <a href="index.html" data-type="index-link">taf-ui documentation</a>
+                    <a href="index.html" data-type="index-link">TAF UI</a>
                 </li>
 
                 <li class="divider"></li>
